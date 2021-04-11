c4f9ae43e8a741370eb910769736d70c000748d8
gissandrogama <gissandrogama@gmail.com>
2021-04-11 11:47:48 -0300

    feat(sheduler): add module availabilities feat(sheduler_web/live): add new functions feat(sheduler_web/live): page_live.html.eex add form

--- lib/scheduler/availabilities.ex
+++ lib/scheduler/availabilities.ex
@@ -1,3 +1,8 @@
 defmodule Scheduler.Availabilities do
+  alias Scheduler.Availabilities.Availability
   def new(), do: %{}
+
+  def validate(params, availavilities) do
+    Availability.changeset(params, availavilities)
+  end
 end
--- lib/scheduler_web/live/page_live.ex
+++ lib/scheduler_web/live/page_live.ex
@@ -1,15 +1,25 @@
 defmodule SchedulerWeb.PageLive do
   use SchedulerWeb, :live_view
-  alias Scheduler.Availabilities.Availability
+  alias Scheduler.Availabilities
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign(changeset: Availability.changeset(%{}, %{}), availabilities: %{})}
+    {:ok, socket |> new |> new_changeset}
+  end
+
+  defp new(socket), do: socket |> assign(availabilities: Availabilities.new())
+
+  def new_changeset(socket) do
+    socket
+    |> assign(changeset: Availabilities.validate(%{}, socket.assigns.availabilities))
   end
 
   @impl true
-  def handle_event("insert-availability", params, socket) do
-    IO.inspect(params)
+  def handle_event("create", %{"availability" => params}, socket) do
+    params
+    |> IO.inspect()
+    socket.assigns.availabilities
+    |> IO.inspect()
     {:noreply, socket}
   end
 end
