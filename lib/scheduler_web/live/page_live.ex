84bc9f238aab0bd640a6351303971ea499a90fcf
gissandrogama <gissandrogama@gmail.com>
2021-04-15 21:31:24 -0300

    feat(live/page_live): add functions that deal with

--- lib/scheduler_web/live/page_live.ex
+++ lib/scheduler_web/live/page_live.ex
@@ -11,15 +11,38 @@ defmodule SchedulerWeb.PageLive do
 
   def new_changeset(socket) do
     socket
-    |> assign(changeset: Availabilities.validate(%{}, socket.assigns.availabilities))
+    |> assign(changeset: Availabilities.validate(socket.assigns.availabilities, %{}))
+  end
+
+  defp validate(socket, availability) do
+    changeset =
+      socket.assigns.availabilities
+      |> Availabilities.validate(availability)
+      |> Map.put(:action, :validate)
+
+      assign(socket, changeset: changeset)
+  end
+
+  defp add_availability(socket, availability) do
+    if socket.assigns.changeset.valid?() do
+      availabilities = socket.assigns.availabilities
+      socket
+      |> assign(availabilities: Availabilities.add_availability(availabilities, availability))
+      |> put_flash(:info, "New Availability created!")
+      |> new_changeset()
+    else
+      socket |> put_flash(:error, "Invalid availability to create!")
+    end
   end
 
   @impl true
-  def handle_event("create", %{"availability" => params}, socket) do
-    params
-    |> IO.inspect()
-    socket.assigns.availabilities
-    |> IO.inspect()
-    {:noreply, socket}
+  def handle_event("create", %{"availability" => availability}, socket) do
+      {:noreply, socket |> add_availability(availability)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"availability" => availability}, socket) do
+    IO.inspect(availability)
+      {:noreply, socket |> validate(availability)}
   end
 end
