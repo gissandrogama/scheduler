25176e072701e3914d0a77d21339b2c92a07182e
gissandrogama <gissandrogama@gmail.com>
2021-04-15 21:28:47 -0300

    feat(scheduler/availabilites): add fuction add_availability

--- lib/scheduler/availabilities.ex
+++ lib/scheduler/availabilities.ex
@@ -2,7 +2,11 @@ defmodule Scheduler.Availabilities do
   alias Scheduler.Availabilities.Availability
   def new(), do: %{}
 
-  def validate(params, availavilities) do
-    Availability.changeset(params, availavilities)
+  def validate(availabilities, params) do
+    Availability.changeset(availabilities, params)
+  end
+
+  def add_availability(availabilities, availability) do
+    availabilities |>  Map.put(availability["week_day"], availability)
   end
 end
