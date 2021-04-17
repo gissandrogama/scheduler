50cf696ac92d517e313021aea03a16b9e0de041f
gissandrogama <gissandrogama@gmail.com>
2021-04-17 00:48:07 -0300

    feat(scheduler/availabilites): add functions

--- lib/scheduler/availabilities.ex
+++ lib/scheduler/availabilities.ex
@@ -1,12 +1,16 @@
 defmodule Scheduler.Availabilities do
   alias Scheduler.Availabilities.Availability
+  alias Scheduler.Availabilities.DayRules
   def new(), do: %{}
 
   def validate(availabilities, params) do
     Availability.changeset(availabilities, params)
   end
 
+  def week_day_list, do: DayRules.day_number()
+  def get_day(number), do: DayRules.get_day(number)
+
   def add_availability(availabilities, availability) do
-    availabilities |>  Map.put(availability["week_day"], availability)
+    availabilities |> Map.put(availability["week_day"], availability)
   end
 end
